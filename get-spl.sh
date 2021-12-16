bf71aa76bcae7f99030e7f13260b721f326f0515
D. Dessy <ddessy@splunk.com>
2021-12-16 11:27:55 +0100

    timestamp

--- get-spl.sh
+++ get-spl.sh
@@ -3,9 +3,10 @@ APP=geomaps
 APP_FILE=${APP}.spl
 APP_LOCATION=/opt/splunk/share/splunk/app_packages/${APP_FILE}
 source .env
-# Ask Splunk to package app
-curl -k -u admin:$SPLUNK_PASSWORD https://localhost:8089/services/apps/local/$APP/package
-# Download the app
+echo "Asking Splunk to package ${APP_FILE} ..."
+curl -s -k -u admin:$SPLUNK_PASSWORD https://localhost:8089/services/apps/local/$APP/package
+echo "Downloading ${APP_FILE} ..."
 docker cp so1:$APP_LOCATION .
-# Expand app
-tar xvf ${APP_FILE} -C apps/
+echo "Expanding ${APP_FILE} ..."
+tar -xf ${APP_FILE} -C apps/
+echo "Done."
